c45e61a35fb6bafdde2415fbec500ccedbcd870e
Habify <Newton.amzl@gmail.com>
2025-01-10 21:01:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const currentDate = new Date();
+
+const year = currentDate.getFullYear(); 
+const month = currentDate.getMonth() + 1; // Months are 0-indexed, so add 1
+const day = currentDate.getDate();
+
+function addDate() {
+        var dayInput = document.getElementById("day");
+        var monthInput = document.getElementById("month");
+        var yearInput = document.getElementById("year");
+        dayInput.textContent  = day;
+        monthInput.textContent  = month;
+        yearInput.textContent  = year;
+        }
+        window.onload = addDate;
