b82b3d5f872a545efdf2e2862fd6e9976c4e92c1
Habify <Newton.amzl@gmail.com>
2025-01-16 13:29:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const currentDate = new Date();
-
-const year = currentDate.getFullYear(); 
-const month = currentDate.getMonth() + 1; // Months are 0-indexed, so add 1
-const day = currentDate.getDate();
 
 
-window.onload = addDate();
 window.onload = addDay();
 
-
-function addDate() {
-        var dayInput = document.getElementById("day");
-        var monthInput = document.getElementById("month");
-        var yearInput = document.getElementById("year");
-        dayInput.textContent  = day;
-        monthInput.textContent  = month;
-        yearInput.textContent  = year;
-        }
 
 
 function addDay() {
@@ -53,7 +38,8 @@ if (currentValue && !isNaN(currentValue)) {
 }
 
 // Store the updated value back in local storage
-  localStorage.setItem("streakCount", currentValue);  
+localStorage.setItem("streakCount", currentValue);  
+  
   dailyStreakOutput.innerHTML= currentValue;
 }
 
@@ -72,6 +58,8 @@ if (currentValue && !isNaN(currentValue)) {
 }
 
 // Store the updated value back in local storage
-  localStorage.setItem("streakCount", currentValue);  
-  dailyStreakOutput.innerHTML= currentValue;
+localStorage.setItem("streakCount", currentValue);  
+  
+dailyStreakOutput.innerHTML= currentValue;
  }
+
